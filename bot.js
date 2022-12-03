dcb0f00041da188924f745a66dbaf024da6c50ba
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2022-12-03 02:42:34 +0100

    Latest changes

--- bot.js
+++ bot.js
@@ -1,24 +1,29 @@
 require('dotenv').config()
 
 const { Client, GatewayIntentBits } = require('discord.js');
-const prefix = '>';
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+const arrayOfStrings = ["/robin", "/Robin"]
 
 client.on('ready', () => {
     console.log('Our Bot is ready')
 })
 
 client.on('messageCreate', (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return
-    
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const command = args.shift().toLowerCase()
+    if (message.author.bot) return // do nothing when the message author is a bot
 
-    // commands
-    if (command === "test") {
-        message.channel.send("Bot is working!")
-    }
+    // check robin commands
+    arrayOfStrings.forEach(string => {
+        if (message.content === string) {
+            message.channel.send("Follow the tutorial posted in the following channel: **#gift-from-robin**")
+        }
+        else if (message.content.substring(0, 6) === string && message.content.length == 16) {
+            message.channel.send("Follow the tutorial posted in the following channel: **#gift-from-robin**")
+        }
+        else if (message.content.substring(0, 6) === string && message.content.length == 17) {
+            message.channel.send("Follow the tutorial posted in the following channel: **#gift-from-robin**")
+        }
+    })
 });
 
 client.login(process.env.BOT_TOKEN)
